4268bbcf2f8609f4ff99e9c3d0fe487286c6e3ff
Clint McGuire <clint@clintmcguire.com>
2013-10-31 15:22:29 -0700

    Initial

---
+++ HyperV-Deploy.ps1
@@ -0,0 +1,5 @@
+﻿#New-VM -Name WS2012MDTTest -MemoryStartupBytes 2GB -NewVHDPath 'C:\Users\Public\Documents\Hyper-V\Virtual hard disks\WS2012MDTTest.vhdx'-NewVHDSize 37580963840 -SwitchName LAN 
+#Add-VMNetworkAdapter -VMName WS2012MDTTest -IsLegacy $true -SwitchName LAN 
+#Set-VM -Name WS2012MDTTest -DynamicMemory -MemoryMinimumBytes 512MB -MemoryMaximumBytes 2048MB -MemoryStartupBytes 2048MB 
+#Get-VM WS2012MDTTest | Set-VMBios -StartupOrder @("LegacyNetworkAdapter", "CD", "IDE", "floppy")
+Get-VM WS2012MDTTest | Start-VM 
---
+++ VM-Deploy.ps1
@@ -0,0 +1,13 @@
+﻿Param(
+    [string]$VMName,
+    [int64]$RAMMin,
+    [int64]$RAMMax,
+    [int64]$RAMStart
+)
+$vSwitch = 'Internal'
+#New-VM -Name $VMName -NewVHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\$VMName.vhdx" -NewVHDSize 37580963840 -SwitchName LAN
+#Add-VMNetworkAdapter -VMName $VMName -IsLegacy $true -SwitchName $vSwitch 
+Set-VM -Name $VMName -DynamicMemory 
+Get-VM $VMName | Set-VM -MemoryMinimumBytes $RAMMin -MemoryMaximumBytes $RAMMax -MemoryStartupBytes $RAMStart
+#Get-VM $VMName | Set-VMBios -StartupOrder @("LegacyNetworkAdapter", "CD", "IDE", "floppy")
+#Get-VM $VMName | Start-VM 
