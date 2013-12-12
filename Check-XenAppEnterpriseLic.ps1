e5a9b3d7bb0474f9263505c0575190b70d752624
clintmc <clint@clintmcguire.com>
2013-12-12 14:28:38 -0800

    added more documentation

--- Check-XenAppEnterpriseLic.ps1
+++ Check-XenAppEnterpriseLic.ps1
@@ -1,9 +1,9 @@
 <#
 .SYNOPSIS
-Reports on Citrix licenses in use for a specific product.
+Reports on Citrix licenses in use for selected products.
 
 .DESCRIPTION
-This script will query your Citrix Licnese server and 
+This script will query your Citrix Licnese server and output the in use and total licenses for individual products.
 
 .NOTES
 Requires Citrix Licensing Snapin (Citrix.Licensing.Admin.V1)
@@ -21,22 +21,26 @@ PS> .\Check-XenAppEnterpriseLic.ps1
 XenApp Enterprise Licenses in use: 90
 
 #>
+############################################################################################
 #DEFINE THESE VARIABLES FOR YOUR ENVIRONMENT
 
 #Enter the URL for your License server, typically this uses HTTPS and port 8083
+#E.G. "https://licensingservername:8083"
 $ServerAddress = 
 
+#Enter the license type you would like to output, this can be a comma separeated list, include each otption in single quotes
+#E.G. 'Citrix XenApp Enterprise','Citrix XenDesktop Enterprise','Citrix EdgeSight for XenApp'
 $LicenseTypeOutput = 
 
-
-
+############################################################################################
 
 #Check for Licensing Snap-in, add if not currently added
+#PowerShell Snap-in is contained in: LicensingAdmin_PowerShellSnapIn_x64.msi
 if ( (Get-PSSnapin -Name Citrix.Licensing.Admin.V1 -ErrorAction SilentlyContinue) -eq $null )
 {
     Add-PsSnapin Citrix.Licensing.Admin.V1
 }
-#PowerShell Snapin is contained in this file LicensingAdmin_PowerShellSnapIn_x64.msi
+
 
 #Create Hash tables
 $LicAll = @{}
@@ -74,6 +78,7 @@ foreach ($LicInfo in $LicAll)
 		}
 }
 
+#Output license usage for each requested type.
 Foreach ($Type in $LicenseTypeOutput)
 {
 	$OutPutLicInUse = $LicInUse.Get_Item($Type)
