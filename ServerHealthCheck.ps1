6a4491c502cf6b4ee828224552a38fa9b93d1671
clintmc <clint@clintmcguire.com>
2014-01-06 15:02:27 -0800

    adding new script

---
+++ ServerHealthCheck.ps1
@@ -0,0 +1,11 @@
+#Server Health Check
+
+
+
+
+
+# E-mail report details
+$emailFrom     = ""
+$emailTo       = ""
+$emailSubject  = ("XenApp Farm Report - " + (Get-Date -format R))
+$smtpServer    = ""
